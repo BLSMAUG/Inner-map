119b5250d5f15dee09582ac83a718737c7375d58
Pango-blimp <maxencedracs@gmail.com>
2026-01-30 19:22:37 +0100

    ScriptMovement

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -2,15 +2,37 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    public float speed = 5f;
+
+    CharacterController Cac;
+
 
     void Start()
     {
-
+        Cac = GetComponent<CharacterController>();
     }
 
 
     void Update()
     {
-        
+        Move();
+    }
+
+    void Move()
+    {
+        float moveX = 0f;
+        float moveZ = 0f;
+
+        if (Input.GetKey(KeyCode.W)) moveZ += 1f;
+        if (Input.GetKey(KeyCode.S)) moveZ -= 1f;
+        if (Input.GetKey(KeyCode.A)) moveX -= 1f;
+        if (Input.GetKey(KeyCode.D)) moveX += 1f;
+
+        Vector3 move = transform.right * moveX + transform.forward * moveZ;
+
+        move = Vector3.ClampMagnitude(move, 1f);
+
+        Cac.Move(move * speed * Time.deltaTime);
+
     }
 }
